8e4964db60946015fcccc09597ee2fb9f15b4f5f
aryansinghchauhan000-ops <aryansinghchauhan000@gmail.com>
2026-09-14 17:32:15 +0530

    half_sub_test_bench.v

--- half_subtractor/half_sub_test_bench.v
+++ half_subtractor/half_sub_test_bench.v
@@ -2,4 +2,22 @@ module tb;
   reg a,b;
   wire diff, borr;
 
+  half_sub abc(a,b,diff,borr);
+
+  initial begin
+    $monitor("Time = %t, a =%b, b =%b, diff = %b, borr = %b", $time, a,b,diff,borr);
+    for(int i = 0; i < 5; i++)
+      begin
+        {a, b} = i;
+        #1;
+      end
+    #1;
+    $finish;
+  end
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0,tb);
+  end
+endmodule
+
   
